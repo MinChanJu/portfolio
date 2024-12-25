dce41d3470e5358e6e17d42226bce78547dd8a56
MinChanJu <mcj00220077@gmail.com>
2024-12-25 11:39:21 +0900

    upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Portfolio from './components/Portfolio'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Portfolio />
+    </div>
+  )
+}
+
+export default App
