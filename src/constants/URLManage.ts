b22e11a264ccb638f20e7f6750a746bf11b072b8
MinChanJu <mcj00220077@gmail.com>
2025-03-11 19:03:38 +0900

    프로젝트 이름 변경

--- src/constants/URLManage.ts
+++ src/constants/URLManage.ts
@@ -2,7 +2,7 @@ export const IMG: { [key: string]: string[] } = {
   "coding_contest": Object.values(import.meta.glob("../assets/image/coding_contest/*.{png,jpg,jpeg,svg}", { eager: true, import: "default" })),
   "mic_app": Object.values(import.meta.glob("../assets/image/mic_app/*.{png,jpg,jpeg,svg}", { eager: true, import: "default" })),
   "portfolio": Object.values(import.meta.glob("../assets/image/portfolio/*.{png,jpg,jpeg,svg}", { eager: true, import: "default" })),
-  "random_chat_app": Object.values(import.meta.glob("../assets/image/random_chat_app/*.{png,jpg,jpeg,svg}", { eager: true, import: "default" })),
+  "dating_app": Object.values(import.meta.glob("../assets/image/dating_app/*.{png,jpg,jpeg,svg}", { eager: true, import: "default" })),
   "trash_game": Object.values(import.meta.glob("../assets/image/trash_game/*.{png,jpg,jpeg,svg}", { eager: true, import: "default" })),
 }
 
@@ -10,7 +10,7 @@ export const PROJECT: { [key: string]: string[][] } = {
   "coding_contest": [["백엔드", 'https://github.com/MinChanJu/mic-spring'], ["프론트엔드", 'https://github.com/MinChanJu/mic-react'], ["사이트", 'https://minchanju.github.io/mic-react/']],
   "mic_app": [["소스코드", 'https://github.com/MinChanJu/MIC-app']],
   "portfolio": [["소스코드", 'https://github.com/MinChanJu/portfolio'], ["사이트", 'https://minchanju.github.io/portfolio/']],
-  "random_chat_app": [["소스코드", 'https://github.com/MinChanJu/BuckTanley']],
+  "dating_app": [["소스코드", 'https://github.com/MinChanJu/BuckTanley']],
   "trash_game": [["소스코드", 'https://github.com/MinChanJu/Trash-Flight-Unity']],
 }
 
