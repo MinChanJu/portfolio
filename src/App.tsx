85afde50312e3d888c6c85cf2bd33f7c7c9c24e0
MinChanJu <mcj00220077@gmail.com>
2025-05-22 23:18:20 +0900

    자동화 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import data from './assets/data.json'
 import { AppRoutes, PageTitle, SideBar } from './components'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './styles'
 
 const App: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false); // 사이드바 열림 여부
 
   return (
-    <HashRouter>
+    <BrowserRouter basename='/portfolio'>
       <PageTitle projects={data.projects} />
       <SideBar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} projects={data.projects} />
       <AppRoutes isSidebarOpen={isSidebarOpen} data={data} />
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
