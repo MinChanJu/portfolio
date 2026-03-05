2901a3936d435c15879eeb99a3f04754c542e354
MinChanJu <mcj00220077@gmail.com>
2026-03-05 23:47:52 +0900

    feat: 빌드 설정에 수동 청크 분할 기능 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,19 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   base: "/portfolio",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (!id.includes("node_modules")) return;
+
+          if (id.includes("swiper")) return "swiper";
+          if (id.includes("highlight")) return "highlight-vendor";
+          if (id.includes("react")) return "react-vendor";
+
+          return "vendor";
+        },
+      },
+    },
+  },
 });
